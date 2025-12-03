6cbbbb659cb5adb971ca71223c9987c42a9bbf79
tobyra7 <cristianrausch@gmail.com>
2025-12-03 16:47:34 +0000

    Fix Prisma client import path

--- lib/actions/resume.actions.ts
+++ lib/actions/resume.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
 import { revalidatePath } from "next/cache";
 
 const prisma = new PrismaClient();
